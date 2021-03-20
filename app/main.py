e879c39c1ee8d628cd91923bab03ce36b991dc76
zhangboyi <15813380401@163.com>
2021-03-20 16:46:51 +0800

    update

--- app/main.py
+++ app/main.py
@@ -13,6 +13,13 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+@app.get("/test")
+def get_items(item_id: int, name: str):
+    return {
+        "code": 200,
+        "item_id": item_id,
+        "name": name
+    }
 
 if __name__ == '__main__':
     import uvicorn
